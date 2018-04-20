45adc8fa1bb749bffee030063398a8857247acdf
cd74 <26689922+cd74@users.noreply.github.com>
2018-04-20 09:51:27 -0400

    Updates and changes
    Added OS Check to pass OS specific ping parameters to the ping command.
    Changed default behaviour to always hide unknown/unreachable hosts.
    Changed the --hide option to --show to support the new default behaviour.

--- ping_sweep.py
+++ ping_sweep.py
@@ -1,17 +1,17 @@
 """
     Ping sweep a network subnet for all hosts.
-    
+
     Display hostname and IP address of active hosts.
+    Default behaviour is to not show non-responsive hosts.
 
     To Do:
         1) Figure out how to sort the output by IP.
            - with multiprocessing the pings with a response are coming
              back much faster, as a result the output is out of order.
-        2) Add Windows support.
 
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 import socket
@@ -19,23 +19,23 @@ import subprocess as sp
 import multiprocessing as mp
 import ipaddress as ip
 import argparse
+from platform import system as p_sys
 
 
 def get_args():
     """
         Get the subnet (if it was passed) from the command line using argparse.
-        
+
         In: None
         Return: subnet
     """
     subnet = ""
 
     parser = argparse.ArgumentParser(description="Ping hosts in a given subnet.")
-    parser.add_argument("subnet", metavar = "1.2.3.4/24", type = str, 
-                        help = "subnet in the form of ipaddress and netmask: 10.0.0.0/24")
-    parser.add_argument("--hide", action = "store_true", 
-                        help = "hide hosts which are not reachable")
-
+    parser.add_argument("subnet", metavar="1.2.3.4/24", type=str,
+                        help="subnet in the form of ipaddress and netmask: 10.0.0.0/24")
+    parser.add_argument("--show", action="store_true",
+                        help="show hosts which are not reachable")
 
     """
         Parses the arguments provided by the user and stores them in the
@@ -44,9 +44,9 @@ def get_args():
     """
     args = parser.parse_args()
     subnet = args.subnet
-    hide = args.hide
+    show = args.show
 
-    return subnet,hide
+    return subnet, show
 
 
 def get_host_list(subnet):
@@ -68,23 +68,25 @@ def get_host_list(subnet):
         quit()
 
 
-def ping_host(x, hide):
+def ping_host(ping_params, host_ip, show):
     """
         Ping all the hosts!
     """
-    _, result = sp.getstatusoutput("ping -c1 " + str(x))
+    status, result = sp.getstatusoutput("ping " + ping_params  + " " + str(host_ip))
 
     try:
-        host_info = socket.gethostbyaddr(str(x))[0]
-    except:
+        # socket.gethostbyaddr returns a tuple if a hostname is found.
+        # No hostname found returns a socket.error
+        host_info = socket.gethostbyaddr(str(host_ip))[0]
+    except socket.error:
         host_info = "** UNKNOWN **"
 
-    if not hide and ("unknown host" in result.lower()):
-        print(f"{str(x)}  :  Unknown Host  :  {host_info}")
-    elif not hide and ("host unreachable" in result.lower()):
-        print(f"{str(x)}  :  Host Unreachable  :  {host_info}")
-    elif ("time" in result.lower()) and not (("host unreachable" in result.lower()) or ("unknown host" in result.lower())):
-        print(f"{str(x)}  :  Host Alive  :  {host_info}")
+    if show and ("unknown host" in result.lower()):
+        print(f"{str(host_ip)}  :  Unknown Host  :  {host_info}")
+    elif show and ("host unreachable" in result.lower()):
+        print(f"{str(host_ip)}  :  Host Unreachable  :  {host_info}")
+    elif (("host unreachable" not in result.lower()) and ("unknown host" not in result.lower())):
+        print(f"{str(host_ip)}  :  Host Alive  :  {host_info}")
 
 
 def main():
@@ -92,15 +94,18 @@ def main():
         Time to get to work!
     """
 
-    subnet, hide = get_args()
+    if p_sys().casefold() == "linux":
+        ping_params = "-c 1"
+    elif p_sys().casefold() == "windows":
+        ping_params = "-n 1"
+    elif p_sys().casefold() == "darwin":
+        ping_params = "-c 1"
+
+    subnet, show = get_args()
     jobs = []
 
-    for x in get_host_list(subnet):
-        try:
-            host_info = socket.gethostbyaddr(str(x))[0]
-        except:
-            host_info = ""
-        p = mp.Process(target=ping_host, args=(str(x), hide))
+    for host_ip in get_host_list(subnet):
+        p = mp.Process(target=ping_host, args=(ping_params, str(host_ip), show))
         jobs.append(p)
         p.start()
 
